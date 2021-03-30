3e382fbe4ee4276f14ca89273786ffc56040cc34
Andrés <funandreth97@gmail.com>
2021-03-30 16:35:15 -0300

    Actualizar 30-3_9

---
+++ 30_3/10.py
@@ -0,0 +1,34 @@
+print("Qué mes es?")
+
+num = int(input("Ingresa un número del 1 al 12: "))
+
+if(num < 1 or num > 12):
+    print("Fuera de rango.")
+else:
+    if num == 1:
+        print("Es Enero.")
+    elif num == 2:
+        print("Es Febrero.")
+    elif num == 3:
+        print("Es Marzo.")
+    elif num == 4:
+        print("Es Abril.")
+    elif num == 5:
+        print("Es Mayo.")
+    elif num == 6:
+        print("Es Junio.")
+    elif num == 7:
+        print("Es Julio.")
+    elif num == 8:
+        print("Es Agosto.")
+    elif num == 9:
+        print("Es Septiembre.")
+    elif num == 10:
+        print("Es Octubre.")
+    elif num == 11:
+        print("Es Noviembre.")
+    elif num == 12:
+        print("Es Diciembre.")
+
+# Termina el programa
+print("-- PROGRAM END --")
