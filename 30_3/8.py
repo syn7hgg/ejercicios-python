b98fff4deafdbed840e4b0ae3d3dc8aeec36354e
Andrés <funandreth97@gmail.com>
2021-03-30 16:07:56 -0300

    Actualizar 30-3_7

---
+++ 30_3/8.py
@@ -0,0 +1,11 @@
+print("Par o Impar?")
+
+num = float(input("Ingrese un número: "))
+
+if num % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
+
+# Termina el programa
+print("-- PROGRAM END --")
