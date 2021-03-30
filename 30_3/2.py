89ec2d4ff6e1b1e59f081191b6cbf4f4771ac057
Andrés <funandreth97@gmail.com>
2021-03-30 15:05:09 -0300

    Actualizar 30-3_3

---
+++ 30_3/2.py
@@ -0,0 +1,31 @@
+a = int(input("Ingresa a: "))
+
+b = int(input("Ingresa b: "))
+
+c = int(input("Ingresa c: "))
+
+if a != 0:
+
+    print("ecuacion de segundo grado")
+
+
+
+else:
+
+    if b != 0:
+
+        print("ecuacion de primer grado")
+
+
+
+    else:
+
+        if c != 0:
+
+            print("la ecuacion no tiene solucion")
+
+
+
+        else:
+
+            print("la ecuacion tiene infinitas soluciones")
---
+++ 30_3/3.py
@@ -0,0 +1,15 @@
+matematicas = 5.2
+
+calculo = 6.0
+
+Introduccion = 5.9
+
+ingenieria = 5.6
+
+if ((matematicas >=5.0) and (calculo>=5.0) and (Introduccion >=5.0) and (ingenieria >= 5.0 )):
+
+  print("se exime de todos los examenes")
+
+else:
+
+  print("hay que sufrir en los examenes")
