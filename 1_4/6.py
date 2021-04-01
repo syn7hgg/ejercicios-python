8995de47c1c04a4c586865735bfeb73ab5a1cb9c
Andrés <funandreth97@gmail.com>
2021-04-01 15:55:55 -0300

    Actualizar 1-4_4

---
+++ 1_4/6.py
@@ -0,0 +1,11 @@
+print("== Cadenas largas ==")
+
+i = input("Ingrese una oración de no más de 40 caracteres: ")
+
+if len(i) > 40:
+    print("La oración es muy larga >:(")
+else:
+    print("La oración está dentro del rango aceptable.")
+
+# Termina el programa
+print("-- PROGRAM END --")
---
+++ 1_4/6_prof.py
@@ -0,0 +1,9 @@
+s = str(input("Ingresa tu cadena de tweeter: \n"))
+
+if len(s)>=40:
+
+  print("Este tweet es demasiado largo!")
+
+else:
+
+  print("tu cadena es valida....")
