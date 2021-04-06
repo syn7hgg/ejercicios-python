6c97608a0f3f86479ac486447eb705c1b94cedf6
Andrés <funandreth97@gmail.com>
2021-04-06 15:46:12 -0400

    Actualizar 6-4_3

--- 6_4/5.py
+++ 6_4/5.py
@@ -5,12 +5,10 @@ y = n
 z = 1
 
 while y > 1:
-    print("valores inicio ciclo de", "n:", n, "y:", y, "z:", z)
-
     z *= y
 
     y -= 1
 
     print("valores al final ciclo de", "n:", n, "y:", y, "z:", z)
 
-print("el resultado del factoria del numero", n, "es", z)
+print("el resultado del factorial del numero", n, "es", z)
