7c282668ac4a2d10a1c9071ed6c8a69592878345
Andrés <funandreth97@gmail.com>
2021-04-08 14:34:27 -0400

    Actualizar 8-4_2

---
+++ 8_4/1.py
@@ -0,0 +1,22 @@
+print('Generar una lista del 0 al 4 y mostrarla por pantalla')
+
+for i in range(5):
+    print(i)
+
+# Fin del bucle
+
+
+print('Lista entre 10 y 20')
+
+for j in range(10, 21):
+    print(j)
+
+# Fin del bucle
+
+
+print('Lista de múltiplos de 3 hasta el 30 inclusive')
+
+for multiploDeTres in range(3, 31, 3):
+    print(multiploDeTres)
+
+# Fin del bucle
