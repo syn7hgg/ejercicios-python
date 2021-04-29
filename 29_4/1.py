3986a55e357eff7a942e73f7b7d87b16f02c53b1
Andrés <funandreth97@gmail.com>
2021-04-29 15:04:38 -0400

    Actualización

---
+++ 29_4/1.py
@@ -0,0 +1,10 @@
+# DEBES INSTALAR NUMPY PARA PODER CORRER ESTE CÓDIGO
+# py -m pip install -U pip numpy
+
+from numpy import array
+
+matriz = array([7, 5, 4, 6])
+
+promedio = matriz.mean()
+
+print("el promedio es: ", promedio)
---
+++ 29_4/2.py
@@ -0,0 +1,7 @@
+diccionario = {"12222333-7": "Luis", "16444555-6": "Diego"}
+
+print(diccionario["12222333-7"])
+
+diccionario["13444555-6"] = "Patricia"
+
+print(diccionario)
